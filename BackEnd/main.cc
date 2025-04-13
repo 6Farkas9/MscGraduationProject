d70339919f6e8260db84cfdfe58b03f47910dce0
6Farkas9 <dgszy00@163.com>
2025-04-13 16:30:08 +0800

    drogon配合vcpkg的编译难度超乎想象

--- BackEnd/main.cc
+++ BackEnd/main.cc
@@ -1,14 +1,22 @@
 #include <drogon/drogon.h>
+#include <torch/script.h>
+#include <iostream>
 
-
+using namespace std;
 
 int main() {
     //Set HTTP listener address and port
-    drogon::app().addListener("127.0.0.1", 80);
+    
+    // drogon::app().addListener("127.0.0.1", 80);
+    
     //Load config file
     //drogon::app().loadConfigFile("../config.json");
     //drogon::app().loadConfigFile("../config.yaml");
     //Run HTTP framework,the method will block in the internal event loop
-    drogon::app().run();
+    
+    // drogon::app().run();
+
+    torch::jit::scr
+
     return 0;
 }
