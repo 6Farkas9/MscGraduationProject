d0d591fe8b3e6b3adf9091897a14acf67aff9616
6Farkas9 <dgszy00@163.com>
2025-06-17 21:37:48 +0800

    更换前端模板

---
+++ FrontEnd/src/views/Components/edit/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <WangEditor v-model="editVal" height="calc(100vh - 180px)" />
+</template>
+
+<script setup lang="ts">
+
+const editVal = ref("Hello! WangEditor")
+watchEffect(() => {
+  console.log(editVal.value)
+})
+</script>
+
+<style scoped></style>
